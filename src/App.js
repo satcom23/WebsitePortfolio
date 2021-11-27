a60ffa1f3f622740b3bc2ecf103366030993b1dd
Jimmy <jimmyojeda94@gmail.com>
2021-11-26 16:38:09 -0800

    changed about and nav colors

--- src/App.js
+++ src/App.js
@@ -10,10 +10,8 @@ function App() {
   return (
     <main className = "text-gray-400 bg-gray-900 body-font">
       <Navbar />
-      <div className = "bio">
-        <About />
-        <Skills />
-      </div>
+      <About />
+      <Skills />
       <Projects />
       <Contact />
     </main>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,27 @@
+import "./style.css"
+
 export default function About() {
     return (
         <div className = "about">
-            <h1>Hi I'm Jimmy <br/> 
-            and I love building elegant 
-            inspiring apps! 
-            </h1>
-            <p>
-                A little bit more about me, I've been passionate about 
-                web development and started to carve my path back in 
-                2016 when I bought my first domain name. Since then I've
-                dabbled in free courses and started to learn on my own but
-                I never took the plunge until Covid-19 hit our world like a 
-                wrecking ball. This is when I took an opportunity to go fulltime
-                into my knowledge and joined a bootcamp. After finishing this bootcamp
-                I began build more and more projects fueling my passion.
+            <div className = "intro">
+                <h1>Hi I'm Jimmy <br/> 
+                and I love building <br/>
+                elegant inspiring apps! 
+                </h1>
+            </div>
+            <div className = "bio">
+                <p>
+                    A little bit more about me, I've been passionate about 
+                    web development and started to carve my path back in 
+                    2016 when I bought my first domain name. Since then I've
+                    dabbled in free courses and started to learn on my own but
+                    I never took the plunge until Covid-19 hit our world like a 
+                    wrecking ball. This is when I took an opportunity to go fulltime
+                    into my knowledge and joined a bootcamp. After finishing this bootcamp
+                    I began build more and more projects fueling my passion.
 
-            </p>
+                </p>
+            </div>
         </div>
         
     )
